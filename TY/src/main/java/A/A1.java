2d6649768cb16ac1aa975a199efde7a25d6df88f
RDRL <RDRL@REL>
2020-08-10 22:17:38 +0530

    raghu

--- TY/src/main/java/A/A1.java
+++ TY/src/main/java/A/A1.java
@@ -1,16 +1,20 @@
 package A;
 
 public class A1 {
-
+	
+/**
+ * 
+ * @Raghaveni
+ */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("step 1");
 		Method1();
-		System.out.println("step 4");
+		System.out.println("step 2");
 	}
 public static void Method1()
 {
-	System.out.println("step 3");
+	System.out.println("method 1 calling");
 	}
 
 	
