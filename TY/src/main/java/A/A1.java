ee9f1bb2906e27466940264d9733bc15dac1a556
RDRL <RDRL@REL>
2020-08-10 19:23:58 +0530

    Raghu

--- TY/src/main/java/A/A1.java
+++ TY/src/main/java/A/A1.java
@@ -6,11 +6,13 @@ public class A1 {
 		// TODO Auto-generated method stub
 		System.out.println("step 1");
 		Method1();
+		System.out.println("step 4");
 	}
 public static void Method1()
 {
 	System.out.println("step 3");
 	}
+
 	
 
 }
